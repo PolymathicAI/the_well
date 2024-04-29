fa29fd8a2bffa8b1293f1c4892c6cbda5db8ec57
Lucas Meyer <lucas.thibaut.meyer@gmail.com>
2024-04-29 22:00:37 +0200

    [#19] Test for downloading active matter dataset

---
+++ tests/test_download.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+import glob
+import os
+
+from download_script import download_files
+
+
+class TestDownload(TestCase):
+    def test_active_matter(self):
+        ACTIVE_MATTTER_DIR = os.path.abspath("2D/active_matter")
+        ACTIVE_MATTTER_DATA_DIR = os.path.join(ACTIVE_MATTTER_DIR, "data")
+
+        self.assertTrue(os.path.isdir(ACTIVE_MATTTER_DIR))
+        self.assertFalse(os.path.isdir(ACTIVE_MATTTER_DATA_DIR))
+        download_files(dataset_name="active_matter", sample_only=True)
+        self.assertTrue(os.path.isdir(ACTIVE_MATTTER_DATA_DIR))
+        hdf5_files = glob.glob(f"{ACTIVE_MATTTER_DATA_DIR}/*.hdf5")
+        self.assertTrue(len(hdf5_files))
