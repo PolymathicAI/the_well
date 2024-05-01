a68591e03f8893e8c17540c715ec9350af9719b8
Lucas Meyer <lucas.thibaut.meyer@gmail.com>
2024-05-01 17:47:39 +0200

    [#2] Use fake dataset for testing purposes

--- the_well/benchmarking/training.py
+++ the_well/benchmarking/training.py
@@ -1,12 +1,13 @@
 import logging
 
 import torch
-from benchmarking.data_utils.datasets import GenericWellDataset
+
+# from benchmarking.data_utils.datasets import GenericWellDataset
 from modulus.models.fno import FNO
 from torch.nn import MSELoss
 from torch.optim import Adam
 from torch.optim.lr_scheduler import StepLR
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, TensorDataset
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -27,7 +28,10 @@ model = model.to(device)
 
 # Data
 path = "..."
-train_dataset = GenericWellDataset(path, well_split_name="train")
+# train_dataset = GenericWellDataset(path, well_split_name="train")
+fake_inputs = torch.rand(10, in_channels, 32, 32)
+fake_outputs = torch.rand(10, out_channels, 32, 32)
+train_dataset = TensorDataset(fake_inputs, fake_outputs)
 batch_size = 4
 shuffle = True
 pin_memory = False
@@ -49,9 +53,11 @@ model.train()
 for epoch in range(max_epochs):
     epoch_loss = 0.0
     for batch in train_dataloader:
-        x = batch.to(device)
-        y = model(x)
-        loss = loss_fn(x, y)
+        x, y_ref = batch
+        x = x.to(device)
+        y_ref = y_ref.to(device)
+        y_pred = model(x)
+        loss = loss_fn(y_ref, y_pred)
         epoch_loss += loss.item() * batch_size / len(train_dataset)
         loss.backward()
         optimizer.step()
