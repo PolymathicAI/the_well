6c3f30a1fbe59f95f30ac2d5e697094655a7e758
Lucas Meyer <lucas.thibaut.meyer@gmail.com>
2024-05-13 20:01:19 +0200

    [#8] Format batch data for FNO

--- the_well/benchmark/models/fno.py
+++ the_well/benchmark/models/fno.py
@@ -1,5 +1,6 @@
-from typing import Optional
+from typing import Dict, Tuple
 
+from einops import rearrange
 import torch
 import torch.nn.functional as F
 from pdearena.modules.conditioned.condition_utils import fourier_embedding
@@ -58,10 +59,22 @@ class FNO(ResNet):
             ]
         )
 
-    def forward(self, x: torch.Tensor, time, z=Optional[torch.Tensor]) -> torch.Tensor:
+    @torch.no_grad()
+    def preprocess_input(
+        self,
+        input: Dict[str, torch.Tensor],
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Retrieve input fields, time and parameters from passed input."""
+        time = input["time"].view(-1)
+        param = input["parameters"]
+        x = input["x"]
+        return x, time, param
+
+    def forward(self, input: Dict[str, torch.Tensor]) -> torch.Tensor:
+        x, time, z = self.preprocess_input(input)
         assert x.dim() == 5
         orig_shape = x.shape
-        x = x.reshape(x.size(0), -1, *x.shape[3:])  # collapse T,C
+        x = rearrange(x, "B T W H C -> B (T C) W H")
 
         emb = self.time_embed(fourier_embedding(time, self.in_planes))
         if z is not None:
@@ -71,15 +84,15 @@ class FNO(ResNet):
                     z.shape[-1] == self.n_param_conditioning
                 ), f"Expected Nx{self.n_param_conditioning} parameters but received {z.shape}"
                 for p_dim in range(z.shape[-1]):
-                    emb = emb + self.pde_emb[p_dim](
+                    p_embedding = self.pde_emb[p_dim](
                         fourier_embedding(z[:, p_dim], self.in_planes)
                     )
+                    emb = emb + p_embedding
         x = self.activation(self.conv_in1(x.float()))
         x = self.activation(self.conv_in2(x.float()))
 
         if self.padding > 0:
             x = F.pad(x, [0, self.padding, 0, self.padding])
-
         for layer in self.layers:
             x = layer(x, emb)
 
@@ -91,9 +104,4 @@ class FNO(ResNet):
 
         if self.diffmode:
             raise NotImplementedError("diffmode")
-        return x.reshape(
-            orig_shape[0],
-            -1,
-            (self.n_output_scalar_components + self.n_output_vector_components * 2),
-            *orig_shape[3:],
-        )
+        return x.reshape(*orig_shape)
