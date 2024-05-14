f30c428f557ccbb58e6964b85a43aabe7d14cd8e
Bruno Regaldo <bruno.regaldo@gmail.com>
2024-05-14 15:38:45 -0400

    FIX linter check

--- tests/test_metric.py
+++ tests/test_metric.py
@@ -2,14 +2,14 @@ from unittest import TestCase
 import torch
 
 from the_well.benchmark.data.datasets import GenericWellMetadata
-import the_well.benchmark.metrics as metrics
+from the_well.benchmark.metrics.spatial import mse, nmse, rmse, nrmse
 
 class TestSpatialMetrics(TestCase):
     def test_distance_to_itself(self):
-        for metric in [metrics.mse,
-                       metrics.nmse,
-                       metrics.rmse,
-                       metrics.nrmse]:
+        for metric in [mse,
+                       nmse,
+                       rmse,
+                       nrmse]:
             x = torch.tensor([1., 2., 3.]).unsqueeze(-1)
             meta = GenericWellMetadata(spatial_ndims=1)
             error = metric(x, x, meta)
