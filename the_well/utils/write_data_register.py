961de9f6a64b08fb682dcd48358838c9d2ee74ea
Lucas Meyer <lucas.thibaut.meyer@gmail.com>
2024-05-15 13:22:45 +0200

    [#47] Run isort and ruff formatting

--- the_well/utils/write_data_register.py
+++ the_well/utils/write_data_register.py
@@ -1,5 +1,6 @@
-import os
 import json
+import os
+
 
 def generate_file_lists(root_dirs):
     """
@@ -9,21 +10,26 @@ def generate_file_lists(root_dirs):
     :param root_dirs: List of directories to index.
     :return: Dictionary of file lists indexed by directory.
     """
-    base_url = 'https://users.flatironinstitute.org/~polymathic/data/the_well/'
+    base_url = "https://users.flatironinstitute.org/~polymathic/data/the_well/"
     all_files = {}
     for root_dir in root_dirs:
         file_list = []
         for root, dirs, files in os.walk(root_dir):
             for file in files:
                 # Append the full path or just the filename
-                file_list.append(os.path.join(root, file).replace('/mnt/home/polymathic/ceph/the_well/', base_url))
+                file_list.append(
+                    os.path.join(root, file).replace(
+                        "/mnt/home/polymathic/ceph/the_well/", base_url
+                    )
+                )
         # Use the last part of the directory path as the key to make it more readable
-        #key = os.path.basename(root_dir) if os.path.basename(root_dir) else os.path.basename(os.path.dirname(root_dir))
-        key = os.path.dirname(root_dir.rstrip('/')).split('/')[-1]
+        # key = os.path.basename(root_dir) if os.path.basename(root_dir) else os.path.basename(os.path.dirname(root_dir))
+        key = os.path.dirname(root_dir.rstrip("/")).split("/")[-1]
         print(key)
 
         all_files[key] = file_list
     return all_files
+
 
 def write_to_json(data, output_file):
     """
@@ -32,19 +38,22 @@ def write_to_json(data, output_file):
     :param data: Data to write (dictionary format).
     :param output_file: Path to the output JSON file.
     """
-    with open(output_file, 'w') as f:
+    with open(output_file, "w") as f:
         json.dump(data, f, indent=4)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # List of directories you want to index
-    directories_to_index = ['/mnt/home/polymathic/ceph/the_well/2D/active_matter/data', 
-                            '/mnt/home/polymathic/ceph/the_well/2D/euler_quadrants/data', 
-                            '/mnt/home/polymathic/ceph/the_well/2D/pattern_formation/data',
-                            '/mnt/home/polymathic/ceph/the_well/2D/turbulent_radiative_layer/data']
+    directories_to_index = [
+        "/mnt/home/polymathic/ceph/the_well/2D/active_matter/data",
+        "/mnt/home/polymathic/ceph/the_well/2D/euler_quadrants/data",
+        "/mnt/home/polymathic/ceph/the_well/2D/pattern_formation/data",
+        "/mnt/home/polymathic/ceph/the_well/2D/turbulent_radiative_layer/data",
+    ]
     # Generate file lists
     directory_files = generate_file_lists(directories_to_index)
     # Output JSON file
-    json_file_path = 'data_registry.json'
+    json_file_path = "data_registry.json"
     # Write to JSON
     write_to_json(directory_files, json_file_path)
     print(f"File lists have been written to {json_file_path}")
