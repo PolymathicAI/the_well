d8e5d88cb6ac6cbc9032486b3477effe1210c9d3
Lucas Meyer <lucas.thibaut.meyer@gmail.com>
2024-05-15 17:05:59 +0200

    [#47] Remove unnecessary tests
    Test were targetting features that are not yet implemented.

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -6,32 +6,14 @@ from the_well.benchmark.data.datasets import GenericWellDataset
 class TestDataset(TestCase):
     def test_local_dataset(self):
         dataset = GenericWellDataset(
-            well_base_path=".", well_dataset_name="active_matter"
-        )
-        self.assertTrue(len(dataset))
-
-    def test_absolute_path_dataset(self):
-        dataset = GenericWellDataset(path="2D/active_matter/data")
-        self.assertTrue(len(dataset))
-
-    def test_no_normalization(self):
-        dataset = GenericWellDataset(
             well_base_path=".",
             well_dataset_name="active_matter",
             use_normalization=False,
         )
         self.assertTrue(len(dataset))
 
-    def test_transform(self):
+    def test_absolute_path_dataset(self):
         dataset = GenericWellDataset(
-            well_base_path=".",
-            well_dataset_name="active_matter",
-            transforms=[lambda x: x],
+            path="2D/active_matter/data/train", use_normalization=False
         )
         self.assertTrue(len(dataset))
-
-    def test_exclude_keys(self):
-        pass
-
-    def test_include_keys(self):
-        pass
