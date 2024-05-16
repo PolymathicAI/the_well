737dae76f912b904856f78c0f73f48ba79167652
Lucas Meyer <lucas.thibaut.meyer@gmail.com>
2024-05-16 17:08:09 +0200

    [#2] Fix validation loss

--- the_well/benchmark/trainer/training.py
+++ the_well/benchmark/trainer/training.py
@@ -45,7 +45,7 @@ class Trainer:
             y_ref = y_ref.to(self.device)
             y_pred = self.model(x)
             loss = self.loss_fn(y_ref, y_pred)
-            validation_loss += loss() * y_ref.size(0) / len(dataloader.dataset)
+            validation_loss += loss * y_ref.size(0) / len(dataloader.dataset)
         if self.is_distributed:
             dist.all_reduce(validation_loss, op=dist.ReduceOp.AVG)
         validation_loss = validation_loss.item()
