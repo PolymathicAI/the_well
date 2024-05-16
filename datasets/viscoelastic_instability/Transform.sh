48187b584f9813423271d879bb50e16d62727bd1
rubenohana <ruben.ohana@hotmail.fr>
2024-05-16 12:03:36 -0400

    first test of reorganization for viscoelastic instability

---
+++ datasets/viscoelastic_instability/Transform.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash -l
+
+#SBATCH --time=20:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=ccm
+#SBATCH --output=output_slurm/%j_output
+#SBATCH --error=output_slurm/%j_error
+
+conda deactivate
+source /mnt/home/rohana/python_envs/nanoGPT/bin/activate
+
+python transform.py
