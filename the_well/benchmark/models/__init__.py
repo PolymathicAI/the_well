c08248b2fa18339851e37c327ae6ca98cfe063c2
Mike McCabe <mike.mccabe210@gmail.com>
2024-05-27 21:45:38 -0400

    DilatedResnet working

--- the_well/benchmark/models/__init__.py
+++ the_well/benchmark/models/__init__.py
@@ -2,5 +2,6 @@ from .fno import FNO
 from .tfno import TFNO
 from .unet_classic import UNetClassic
 from .unet_convnext import UNetConvNext
+from .dilated_resnet import DilatedResNet
 
-__all__ = ["FNO", "TFNO", "UNetClassic", "UNetConvNext"]
+__all__ = ["FNO", "TFNO", "UNetClassic", "UNetConvNext", "DilatedResNet"]
