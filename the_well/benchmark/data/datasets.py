cf32f287dddb865a21639e397091129bacc32303
Mike McCabe <mike.mccabe210@gmail.com>
2024-06-03 21:53:39 -0400

    Lint and add comments

--- the_well/benchmark/data/datasets.py
+++ the_well/benchmark/data/datasets.py
@@ -280,6 +280,7 @@ class GenericWellDataset(Dataset):
             with h5.File(file, "r") as _f:
                 # Run sanity checks - all files should have same ndims, size_tuple, and names
                 samples: int = _f.attrs["n_trajectories"]
+                # Number of steps is always last dim of time
                 steps = _f["dimensions"]["time"].shape[-1]
                 size_tuple = [
                     _f["dimensions"][d].shape[0]
@@ -548,17 +549,13 @@ class GenericWellDataset(Dataset):
         if "time_grid" in self.constant_cache:
             time_grid = self.constant_cache["time_grid"]
         elif file["dimensions"]["time"].attrs["sample_varying"]:
-            time_grid = torch.tensor(
-                file["dimensions"]["time"][
-                    sample_idx, :
-                ]
-            )
-            
+            time_grid = torch.tensor(file["dimensions"]["time"][sample_idx, :])
+
         else:
             time_grid = torch.tensor(file["dimensions"]["time"][:])
             self._check_cache("time_grid", time_grid)
-        # Use actual timesteps in case we eventually decide to support non-uniform in time
-        time_grid = time_grid[..., time_idx : time_idx + n_steps * dt : dt]
+        # We have already sampled leading index if it existed so timegrid should be 1D
+        time_grid = time_grid[time_idx : time_idx + n_steps * dt : dt]
         # Nothing should depend on absolute time - might change if we add weather
         time_grid = time_grid - time_grid.min()
 
