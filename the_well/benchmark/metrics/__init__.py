ffe65e802973268d657659553ae4d556b5608710
Mike McCabe <mike.mccabe210@gmail.com>
2024-09-24 15:51:01 -0400

    Splitting video into separate list due to separate inputs

--- the_well/benchmark/metrics/__init__.py
+++ the_well/benchmark/metrics/__init__.py
@@ -22,4 +22,5 @@ __all__ = [
 long_time_metrics = ["VRMSE", "RMSE", "binned_spectral_mse"]
 validation_metric_suite = [RMSE(), NRMSE(), LInfinity(), VRMSE(), binned_spectral_mse()]
 validation_plots = [plot_power_spectrum_by_field, field_histograms]
-time_plots = [plot_all_time_metrics, make_video]
+time_plots = [plot_all_time_metrics]
+time_space_plots = [make_video]
