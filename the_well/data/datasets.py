294bd618ec0f96427667de58418acc488714e9a3
Mike McCabe <mike.mccabe210@gmail.com>
2025-01-07 13:18:13 -0500

    Change spliut name to literal

--- the_well/data/datasets.py
+++ the_well/data/datasets.py
@@ -247,7 +247,7 @@ class WellDataset(Dataset):
         normalization_path: str = "../stats.yaml",
         well_base_path: Optional[str] = None,
         well_dataset_name: Optional[str] = None,
-        well_split_name: Optional[str] = None,
+        well_split_name: Literal["train", "valid", "test", None] = None,
         include_filters: List[str] = [],
         exclude_filters: List[str] = [],
         use_normalization: bool = False,
