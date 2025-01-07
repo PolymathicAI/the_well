1445b05b3d7cb69c4254399c754e1e6bbdb90068
Mike McCabe <mike.mccabe210@gmail.com>
2025-01-07 13:18:54 -0500

    Fixed test to pass split

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -57,6 +57,7 @@ class TestDataset(TestCase):
         dataset = WellDataset(
             well_base_path="datasets",
             well_dataset_name="active_matter",
+            well_split_name="train",
             use_normalization=False,
         )
         self.assertTrue(len(dataset))
