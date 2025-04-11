a86893c80ec894a0e99c8b6f27957d91e0893efc
Lucas Meyer <LTMeyer@users.noreply.github.com>
2025-04-11 17:23:25 +0200

    Fix missing statistics (#44)
    * Add missing statistics
    
    * Remove try-except block causing silent failure
    
    * Add DeltaWellDataset to the list of data imports
    
    * Add dataset tests to check delta statistics
    
    * Round statistics to 4 decimal places
    
    * Fix argument in round function
    
    * Make compute statistics script parallel
    
    * Write stats with 4 decimal scientific notation
    
    * Edit yaml dumping for scientific notation
    
    * Factorize dataset download tests with fixtures
    
    * Reorganize dataset tests
    
    * Add comments to pytest fixtures
    
    * Simplify step selection
    
    * Raise error when stride and normalization are set

--- the_well/data/__init__.py
+++ the_well/data/__init__.py
@@ -1,6 +1,12 @@
 from .augmentation import Augmentation
 from .datamodule import WellDataModule
-from .datasets import WellDataset
+from .datasets import DeltaWellDataset, WellDataset
 from .utils import WELL_DATASETS
 
-__all__ = ["Augmentation", "WellDataModule", "WellDataset", "WELL_DATASETS"]
+__all__ = [
+    "Augmentation",
+    "DeltaWellDataset",
+    "WELL_DATASETS",
+    "WellDataModule",
+    "WellDataset",
+]
