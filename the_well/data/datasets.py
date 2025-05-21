49982f12deaa2eeed604913f0d38ae5cdc938f72
Florian Wiesner <65916600+FloWsnr@users.noreply.github.com>
2025-05-21 10:11:53 -0400

    Update the_well/data/datasets.py
    Co-authored-by: Lucas Meyer <LTMeyer@users.noreply.github.com>

--- the_well/data/datasets.py
+++ the_well/data/datasets.py
@@ -137,8 +137,7 @@ class WellDataset(Dataset):
         normlization_type:
             What type of dataset normalization. Callable Options: ZSCORE and RMS
         max_rollout_steps:
-            Maximum number of output steps to return in a single sample, mostly used for
-            full trajectory mode.
+            Maximum number of output steps to return in a single sample. Return the full trajectory if larger than its actual length.
         n_steps_input:
             Number of steps to include in each sample
         n_steps_output:
