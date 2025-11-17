21e51f6313ccd520e1f0af78e7f74ec649e8b80a
AnnihilatorChess <150355206+AnnihilatorChess@users.noreply.github.com>
2025-11-17 11:00:37 +0100

    actually use the FNO blocks

--- the_well/benchmark/models/fno/__init__.py
+++ the_well/benchmark/models/fno/__init__.py
@@ -47,7 +47,7 @@ class NeuralOpsCheckpointWrapper(neuralop_FNO):
             x = self.domain_padding.pad(x)
 
         for layer_idx in range(self.n_layers):
-            self.optional_checkpointing(
+            x = self.optional_checkpointing(
                 self.fno_blocks, x, layer_idx, output_shape=output_shape[layer_idx]
             )
 
